ee1bf727720409a5cf4caefa71360db998f7bdb3
Roshitha Ignatius <>
2021-05-28 08:06:04 +0000

    init cowin.py

--- cowin.py
+++ cowin.py
@@ -3,51 +3,44 @@ import requests
 from datetime import datetime, timedelta
 import time
 
-def get_slots_availablity(age, pincodes, num_days):
-
-  print_flag = 'Y'
-
-  print("Starting search for Covid vaccine slots!")
+def get_slots_availablity(age, pincode, num_days, dose):
 
   actual = datetime.today()
   list_format = [actual + timedelta(days=i) for i in range(num_days)]
   actual_dates = [i.strftime("%d-%m-%Y") for i in list_format]
 
-  counter = 0   
+  for given_date in actual_dates:
 
-  for pincode in pincodes:   
-      for given_date in actual_dates:
+      counter = 0   
+      available_centers = []
 
-          URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, given_date)
-          header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
-          
-          result = requests.get(URL, headers=header)
+      URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, given_date)
+      header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
+      
+      result = requests.get(URL, headers=header)
 
-          if result.ok:
-              response_json = result.json()
-              if response_json["centers"]:
-                  if(print_flag.lower() =='y'):
-                      for center in response_json["centers"]:
-                          for session in center["sessions"]:
-                              if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
-                                  print('Pincode: ' + pincode)
-                                  print("Available on: {}".format(given_date))
-                                  print("\t", center["name"])
-                                  print("\t", center["block_name"])
-                                  print("\t Price: ", center["fee_type"])
-                                  print("\t Availablity : ", session["available_capacity"])
-
-                                  if(session["vaccine"] != ''):
-                                      print("\t Vaccine type: ", session["vaccine"])
-                                  print("\n")
-                                  counter = counter + 1
-          else:
-              print("No Response!")
-              
-  if not counter:
-      print("No Vaccination slot available!")
-  else:
-      print("Search Completed!")
+      if result.ok:
+          response = result.json()
+          for center in response["centers"]:
+              sessionCounter = 0
+              for session in center["sessions"]:
+                  if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
+                      if session["available_capacity_dose"+str(dose)] > 0 :
+                          available_centers[counter] = {}
+                          available_centers[counter]["center_name"] = center["name"]
+                          available_centers[counter]["center_address"] = center["address"]
+                          available_centers[counter]["center_pincode"] = center["pincode"]
+                          available_centers[counter]["center_id"] = center["center_id"]
+                          available_centers[counter]["fee"] = center["fee_type"]
+                          
+              counter = counter + 1
+      else:
+          return "no-response"
+                  
+      if not counter:
+          print("No Vaccination slot available!")
+      else:
+          print("Search Completed!")
 
   # dt = datetime.now() + timedelta(minutes=3)
 
