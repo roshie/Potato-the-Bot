dfaf1dc3373be4b61d52210f1e7d437cf0886303
RoshithaIgnatiu <>
2021-05-28 07:25:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+import requests 
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Im Online! as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('pt'):
+    await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
